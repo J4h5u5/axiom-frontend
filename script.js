0831f5469c03935ee1e4a986926e9fc2b9358d2d
Max Axenov <maksenov@wavesplatform.com>
2023-03-23 22:17:51 +0300

    feat: check is user exist

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ let user;
 
 enc = new TextEncoder();
 
-fetch(`${API_URL}/usersCount`).then(res => {
+fetch(`${API_URL}/usersCount`).then((res) => {
     res.json().then(({ data }) => {
         document.querySelector('#users-count').innerHTML = data.usersCount;
-    })
-})
+    });
+});
+
+fetch(`${API_URL}/users/${Base58.encode(enc.encode('832611w37'))}`).then((res) => {
+    res.json().then(({ data }) => {
+        console.log(data);
+    });
+});
 
 function onTelegramAuth(user) {
     user = user;
@@ -27,7 +33,20 @@ function onTelegramAuth(user) {
 
     fetch(`${API_URL}/users/${Base58.encode(enc.encode(user.id))}`).then((res) => {
         res.json().then(({ data }) => {
-            console.log(data);
-        })
+            if (!data.user) {
+                fetch(
+                    `${API_URL}/users/)}`,
+                    {
+                        method: POST,
+                        body: JSON.stringify({
+                            "referralId": Base58.encode(enc.encode(user.id)),
+                            "userName":userName
+                        })
+                    })
+                    .then((res) => {});
+            } else {
+                console.log('ee')
+            }
+        });
     });
 }
