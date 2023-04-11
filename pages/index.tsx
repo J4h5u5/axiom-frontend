1db87f427a4b62315e12bbf4094771d79054bf8b
Max Axenov <maksenov@wavesplatform.com>
2023-04-11 22:34:16 +0300

    widget try

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { TLoginButton, TLoginButtonSize } from 'react-telegram-auth';
 import { API_URL } from '../apiUrls';
 import { ITgUserData } from '../apiService/interface';
 import { login } from '../utils/login';
-import TgWidget from '../components/TgWidget/TgWidget';
+import Script from 'next/script';
 
 
 export const getStaticProps = async () => {
@@ -20,6 +20,8 @@ const Home: React.FC<{ usersCount: number }> = ({ usersCount }) => {
     const router = useRouter();
     const [pageState, setPageState] = React.useState<'init' | 'success' | 'error'>('init');
 
+
+
     const onAuth = React.useCallback(async (userData: ITgUserData) => {
         try {
             await login(userData);
@@ -29,6 +31,20 @@ const Home: React.FC<{ usersCount: number }> = ({ usersCount }) => {
             setPageState('error');
         }
     }, []);
+
+    React.useEffect(() => {
+        window.TelegramLoginWidget = {
+            dataOnauth: async (userData: ITgUserData) => {
+                try {
+                    await login(userData);
+                    setPageState('success');
+                    router.push('/launge');
+                } catch (error) {
+                    setPageState('error');
+                }
+            }
+          }
+    }, [])
 
     return (
         <div className="container">
@@ -53,10 +69,45 @@ const Home: React.FC<{ usersCount: number }> = ({ usersCount }) => {
                                     <span className="form__input-content-description">ДАТА: [Уточняется]</span>
                                 </div>
                             </div>
-                            <div className="auth-wrapper">
+                            <div className="auth-wrapper" id="authWrapper">
                                 <button className="form__btn">ПОЕХАЛИ!</button>
                                 <button className="forn__btn_hover">Регистрация в ТГ</button>
-                                <TgWidget botName="AxiomAuthBot" dataOnauth={onAuth} />
+
+                            <Script
+                                strategy="afterInteractive"
+                                dangerouslySetInnerHTML={{
+                                __html: `
+                                (function (d, s, id) {
+                                    console.log(s)
+                                        var js,
+                                            el = d.getElementsByTagName(s)[0];
+                                        if (d.getElementById(id)) {
+                                            return;
+                                        }
+                                        js = d.createElement(s);
+                                        js.async = true;
+                                        js.id = id;
+
+                                        js.src = 'https://telegram.org/js/telegram-widget.js?21'
+                                        js.setAttribute('data-telegram-login', 'AxiomAuthDevBot')
+
+
+                                        // if (cornerRadius !== undefined) {
+                                        //     js.setAttribute('data-radius', cornerRadius.toString())
+                                        // }
+
+                                        js.setAttribute('data-request-access', 'write')
+
+                                        js.setAttribute('data-userpic', 'false')
+                                        js.setAttribute('data-onauth', 'TelegramLoginWidget.dataOnauth(user)')
+
+                                        js.charset = "UTF-8";
+                                        console.log(d.querySelector('#authWrapper'))
+                                        d.querySelector('#authWrapper').appendChild(js, el);
+                                })(document, "script");
+                                `,
+                            }}
+                            />
                             </div>
                         </div>
                         <div className="countdown hidden">7</div>
