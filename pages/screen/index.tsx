d6a932cbf671e29fdc50ce60f634a24eef0c8847
Axepuff <axepuff@gmail.com>
2023-04-21 23:16:08 +0300

    Update departures table (#22)
    * update departure table
    
    * fix
    
    * fix styles
    
    ---------
    
    Co-authored-by: Max Malyavin <max_malyavin@mail.ru>
    Co-authored-by: Max Axenov <maksenov@wavesplatform.com>

--- pages/screen/index.tsx
+++ pages/screen/index.tsx
@@ -1,266 +1,49 @@
-import React, { useState } from "react";
+import React, { memo, useEffect, useRef } from "react";
 import MainContainer from "../../components/MainContainer/MainContainer";
 import withAuth from "../../hocs/WithAuth";
-import Modal from "react-modal";
-import styles from "./screen.module.css";
-import Image from "next/image";
+import { DepartureBoard } from "./departureBoard";
 
 type Props = {};
 
-const Screen = (props: Props) => {
-  const [isOpenModal, setIsOpenModal] = useState(false);
+const Screen = memo((props: Props) => {
+    const ref = useRef<HTMLDivElement | null>();
+    const isRenderedTable = useRef<boolean>(false);
 
-  const onClick = () => {
-    setIsOpenModal((prev) => !prev);
-  };
+    useEffect(() => {
+        if (ref.current && !isRenderedTable.current) {
+            const board = new DepartureBoard(
+                document.getElementById("departure-table"),
+                { rowCount: 4, letterCount: 40 }
+            );
 
-  return (
-    <MainContainer className="auth screen" backRoute="lounge">
-      <div className="wrapper_departure">
-        <div className="inner_departure">
-          <h1 className="screen-title">
-            <span className="plane-departure" /> Departures
-          </h1>
-          <div className="departure-board">
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-          </div>
-          <div className="departure-board" onClick={onClick}>
-            <span className="animated letter letter-T"></span>
-            <span className="animated letter letter-R"></span>
-            <span className="animated letter letter-3"></span>
-            <span className="animated letter letter-0"></span>
-            <span className="animated letter letter-0"></span>
-            <span className="letter letter-blank"></span>
-            <span className="animated letter letter-Я"></span>
-            <span className="animated letter letter-blank"></span>
-            <span className="animated letter letter-Д"></span>
-            <span className="animated letter letter-У"></span>
-            <span className="animated letter letter-М"></span>
-            <span className="animated letter letter-А"></span>
-            <span className="animated letter letter-Ю"></span>
-            <span className="animated letter letter-blank"></span>
-            <span className="animated letter letter-Ч"></span>
-            <span className="animated letter letter-Т"></span>
-            <span className="animated letter letter-О"></span>
-            <span className="letter letter-blank"></span>
-            <span className="animated letter letter-В"></span>
-            <span className="animated letter letter-С"></span>
-            <span className="animated letter letter-Ё"></span>
-            <span className="letter letter-blank"></span>
-            <span className="animated letter letter-М"></span>
-            <span className="animated letter letter-О"></span>
-            <span className="animated letter letter-З"></span>
-            <span className="animated letter letter-Г"></span>
-            <span className="animated letter letter-О"></span>
-            <span className="animated letter letter-Ё"></span>
-            <span className="animated letter letter-Б"></span>
-            <span className="animated letter letter-С"></span>
-            <span className="animated letter letter-Т"></span>
-            <span className="animated letter letter-В"></span>
-            <span className="animated letter letter-О"></span>
-          </div>
-          <div className="departure-board" onClick={onClick}>
-            <span className="animated letter letter-T"></span>
-            <span className="animated letter letter-R"></span>
-            <span className="animated letter letter-3"></span>
-            <span className="animated letter letter-0"></span>
-            <span className="animated letter letter-1"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="animated letter letter-В"></span>
-            <span className="letter letter-blank"></span>
-            <span className="animated letter letter-З"></span>
-            <span className="animated letter letter-А"></span>
-            <span className="animated letter letter-Л"></span>
-            <span className="animated letter letter-Е"></span>
-            <span className="animated letter letter-blank"></span>
-            <span className="animated letter letter-О"></span>
-            <span className="animated letter letter-Ж"></span>
-            <span className="animated letter letter-И"></span>
-            <span className="animated letter letter-Д"></span>
-            <span className="animated letter letter-А"></span>
-            <span className="animated letter letter-Н"></span>
-            <span className="animated letter letter-И"></span>
-            <span className="animated letter letter-Я"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-          </div>
-          <div className="departure-board" onClick={onClick}>
-            <span className="animated letter letter-T"></span>
-            <span className="animated letter letter-R"></span>
-            <span className="animated letter letter-3"></span>
-            <span className="animated letter letter-0"></span>
-            <span className="animated letter letter-2"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="animated letter letter-В"></span>
-            <span className="animated letter letter-Ы"></span>
-            <span className="animated letter letter-П"></span>
-            <span className="animated letter letter-О"></span>
-            <span className="animated letter letter-Л"></span>
-            <span className="animated letter letter-Н"></span>
-            <span className="animated letter letter-Я"></span>
-            <span className="animated letter letter-Е"></span>
-            <span className="animated letter letter-Т"></span>
-            <span className="letter letter-blank"></span>
-            <span className="animated letter letter-Р"></span>
-            <span className="animated letter letter-О"></span>
-            <span className="animated letter letter-Л"></span>
-            <span className="animated letter letter-Ь"></span>
-            <span className="letter letter-blank"></span>
-            <span className="animated letter letter-П"></span>
-            <span className="animated letter letter-О"></span>
-            <span className="animated letter letter-Г"></span>
-            <span className="animated letter letter-Р"></span>
-            <span className="animated letter letter-У"></span>
-            <span className="animated letter letter-Ж"></span>
-            <span className="animated letter letter-Е"></span>
-            <span className="animated letter letter-Н"></span>
-            <span className="animated letter letter-И"></span>
-            <span className="animated letter letter-Я"></span>
-            <span className="letter letter-blank"></span>
-          </div>
-          <div className="departure-board" onClick={onClick}>
-            <span className="animated letter letter-T"></span>
-            <span className="animated letter letter-R"></span>
-            <span className="animated letter letter-3"></span>
-            <span className="animated letter letter-0"></span>
-            <span className="animated letter letter-3"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="animated letter letter-К"></span>
-            <span className="animated letter letter-А"></span>
-            <span className="animated letter letter-К"></span>
-            <span className="letter letter-blank"></span>
-            <span className="animated letter letter-И"></span>
-            <span className="letter letter-blank"></span>
-            <span className="animated letter letter-Е"></span>
-            <span className="animated letter letter-З"></span>
-            <span className="animated letter letter-Д"></span>
-            <span className="animated letter letter-А"></span>
-            <span className="letter letter-blank"></span>
-            <span className="animated letter letter-В"></span>
-            <span className="letter letter-blank"></span>
-            <span className="animated letter letter-Л"></span>
-            <span className="animated letter letter-И"></span>
-            <span className="animated letter letter-Ф"></span>
-            <span className="animated letter letter-Т"></span>
-            <span className="animated letter letter-Е"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-          </div>
-          <div className="departure-board">
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-            <span className="letter letter-blank"></span>
-          </div>
-        </div>
-      </div>
-      <Modal
-        isOpen={isOpenModal}
-        ariaHideApp={false}
-        style={{
-          overlay: {
-            backgroundColor: "none",
-            zIndex: 1,
-          },
-        }}
-        onRequestClose={onClick}
-        className={styles.modal_content}
-      >
-        <div className={`${styles.modal_content_title} text-text`}>
-          Доброе утро, мои Герои!
-        </div>
-        <div className={`${styles.modal_content_desc} text-text`}>
-          Рассказываем о подвигах и занимаем места на пьедестале.
-        </div>
-        <div className={`${styles.modal_content_desc} text-text`}>
-          А мне пора на посадку…
-        </div>
-        {/* // Нужно использовать компонент Image из next */}
-        <div className={`${styles.modal_content_img}`}>
-          <img src="/modal_img.png" alt="modal picture" />
-        </div>
-      </Modal>
-    </MainContainer>
-  );
-};
+            board.setValue([
+                "TR300 Я ДУМАЮ ЧТО ВСЕ МОЗГОЕБСТВО",
+                "TR301 В ЗАЛЕ ОЖИДАНИЯ",
+                "TR302 ВЫПОЛНЯЕТ РОЛЬ ПОГРУЖЕНИЯ",
+                "TR303 КАК ЕЗДА В ЛИФТЕ",
+            ]);
+
+            isRenderedTable.current = true;
+        }
+    }, [ref]);
+
+    return (
+        <MainContainer className="auth screen" backRoute="lounge">
+            <div className="wrapper_departure">
+                <div className="inner_departure">
+                    <h1 className="screen-title">
+                        <span className="plane-departure" /> Departures
+                    </h1>
+                    <div
+                        id="departure-table"
+                        ref={(elem) => (ref.current = elem)}
+                    ></div>
+                </div>
+            </div>
+        </MainContainer>
+    );
+});
+
+Screen.displayName = 'Screen';
 
 export default withAuth(Screen);
